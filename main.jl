58e2e45eaceb768125068202cf9bb03eed49a43e
Samuel Ridler <samridler@hotmail.com>
2024-04-16 22:59:24 +1200

    Clean up script

--- main.jl
+++ main.jl
@@ -16,33 +16,33 @@ ruleevalpass = rand(m, n) .<= 0.9
 
 # todo:
 # - If a rule passes for all candidates then it can be removed,
-#   as rule should be evalated last.
+#   as rule should be evaluated last.
 # - If a candidate passes all rules then it can be removed,
 #   as rule order will not affect rule eval time for the candidate.
 
-@enum SolutionMethod thumb insert repeatinsert swap reinsert ip
+@enum SolutionMethod none thumb insert repeatinsert swap reinsert ip
 
 ruleevalorders = Dict{SolutionMethod,Vector{Int64}}()
 
+ruleevalorders[none] = [1:n;]
 @time ruleevalorders[thumb] = ruleofthumb(ruleevaltimes, ruleevalpass)
 @time ruleevalorders[insert] = insertheuristic(ruleevaltimes, ruleevalpass)
 @time ruleevalorders[repeatinsert] = repeatedinsertheuristic(ruleevaltimes, ruleevalpass)
 @time ruleevalorders[swap] = swapheuristic(ruleevaltimes, ruleevalpass)
 @time ruleevalorders[reinsert] = reinsertheuristic(ruleevaltimes, ruleevalpass)
-# @time ruleevalorders[reinsert] = reinsertheuristic2(ruleevaltimes, ruleevalpass)
 
 # GLPK seems faster than Cbc for this problem
 # optimizer = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0)
 optimizer = optimizer_with_attributes(GLPK.Optimizer, "mip_gap" => 0.0)
 # @time ruleevalorders[ip] = solveip(ruleevaltimes, ruleevalpass, optimizer)
-# t = calcruleevaltime(ruleevaltimes, ruleevalpass, ruleevalorders[insert])
-# @time ruleevalorders[ip] = solveip(ruleevaltimes, ruleevalpass, optimizer, objmax=t) # solving can be slower or faster with objmax
+# objmax = calcruleevaltime(ruleevaltimes, ruleevalpass, ruleevalorders[thumb])
+# @time ruleevalorders[ip] = solveip(ruleevaltimes, ruleevalpass, optimizer, objmax=objmax) # solving can be slower or faster with objmax
 
-@show calcruleevaltime(ruleevaltimes, ruleevalpass, [1:n;])
+print("Solution method, total rule evaluation time:\n")
 for solutionmethod in instances(SolutionMethod)
     if !haskey(ruleevalorders, solutionmethod)
         continue
     end
     t = calcruleevaltime(ruleevaltimes, ruleevalpass, ruleevalorders[solutionmethod])
-    print(solutionmethod, ": ", t, "\n")
+    print(solutionmethod, ": ", round(t, digits=2), "\n")
 end
